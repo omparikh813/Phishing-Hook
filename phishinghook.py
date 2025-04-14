557d927f4c10ceaae85b2608a434e08f614e86ea
Om Parikh <omparikh813@gmail.com>
2025-04-13 22:30:07 -0400

    Created code to retrieve incoming email by specific user and parsed contents

--- phishinghook.py
+++ phishinghook.py
@@ -5,37 +5,85 @@ import os
 from google import genai
 from google.genai import types
 import vt
+import imaplib
+import re
+from email import parser
+from email.policy import default
+import time
 
+#How program works
+print("""\nHello user, we will need to gather some insight into your malicious situation. 
+      \nPlease enter your email to allow me to gather information and then forward the email suspected of malicious activity to [your configured mailing account].\n""")
 
 #Gathering User Insight
-print("""Hello user, we will need to gather some insight into your malicious situation. 
-      \n Please answer the following questions with as mcuh detail as possible.
-      Type not sure if there is no clear answer. """)
-
-persona = input("Who are you? (Ex: Student, Job Position, etc.): ")
-values = input("What potentially valuable information do you hold? (Passwords, Money, Corporate Secrets, etc.): ")
-vectors = input("How could an attacker have found you? (Public Relations, Compromised Email, Website Sign-Ups): )")
-
-email = input("Now enter the suspected email, including any webpage links attached: ")
-
-#Parse input for attachments and plug them into virustotal, add score into prompt
+email = input("Enter your email: ")
 
 
+#Configuring email server and logging into access email
+imap_server=imaplib.IMAP4_SSL('imap.gmail.com')
+imap_server.login('local.lock.app@gmail.com', os.environ.get('EMAIL_PASSWORD'))
+
+#Starts time to find email
+print('Waiting for forwarded email...')
+start_time = time.time()
+
+#Loops until email is found
+while True:
+    current_time = time.time()
+    imap_server.select('INBOX')
+    result, data = imap_server.search(None, 'FROM "{}"'.format(email))
+
+    #Attempts to find lastest email sent by user
+    try:
+        message_id = data[0].split()[-1]
+        break
+
+    except:
+        #Gives two minute time limit to retrieve email
+        if current_time - start_time >= 120:
+            print('Time to send email exceeded. Stopping program now.')
+            quit()
+
+    time.sleep(10)
+
+
+#Retrieves the full message in the raw email format.
+result, data = imap_server.fetch(message_id, 'RFC822')
+raw_email = data[0][1].decode('utf-8')
+
+#Closes server when data is retrieved
+imap_server.close()
+
+#Converts email into Email object
+msg = parser.Parser(policy=default).parsestr(raw_email)
+forwarded_msg = msg.get_payload()[0].get_payload()
+
+#Parse email for attachments andsuspected threat actor
+from_email = re.search(r'From: .* <([\w\.]*@\w*\.\w*)>', forwarded_msg)
+links = re.findall(r'https?://[\w.]*', forwarded_msg)
+links = list(set(links))
+
+print(links)
 
 #Virus Total Assessment
 client = vt.Client(os.environ.get("VT_API_KEY"))
 
+
+
 #Creating prompt for comprehensive analysis
-prompt = """I am a {} who has recently recieved an email that I suspect of malicious intent. 
-I hold {}, which are potentially valuable to attackers. If the email was of malicious intent, 
-the attacker might have reached me from {}. Please analyze the email in a single concise pargraph,
-and determine if it is likely of malicious intent, or if it is safe to interact with.
-The email is attached below. \n {}""".format(persona, values, vectors, email)
+user_insight = ""
+
+
+# prompt = """I am a {} who has recently recieved an email that I suspect of malicious intent. 
+# I hold {}, which are potentially valuable to attackers. If the email was of malicious intent, 
+# the attacker might have reached me from {}. Please analyze the email in a single concise pargraph,
+# and determine if it is likely of malicious intent, or if it is safe to interact with.
+# The email is attached below. \n {}""".format(persona, values, vectors, email)
 
 
 
 #Defines scope of gemini and inputs prompt
-def generate():
+def generate(prompt):
     client = genai.Client(
         api_key=os.environ.get("GEMINI_API_KEY"),
     )
@@ -62,4 +110,5 @@ def generate():
 
 #Executes text generation
 if __name__ == "__main__":
-    generate()
+    #generate("No prompt yet, just testing")
+    pass
